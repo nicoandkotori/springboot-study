6d2a07c1d43f8b5346a71c1a8b1802f9b1837e61
nicoandkotori <806768974@qq.com>
2022-09-26 00:29:36 +0800

    简单装饰者模式实现

---
+++ src/main/java/com/example/springbootstudy/responsibility/Handler.java
@@ -0,0 +1,11 @@
+package com.example.springbootstudy.responsibility;
+
+/**
+ * 抽象处理类
+ * @author mijiahao
+ * @date 2022/09/18
+ */
+public abstract class Handler {
+
+
+}
---
+++ src/main/java/com/example/springbootstudy/responsibility/LeaveRequest.java
@@ -0,0 +1,19 @@
+package com.example.springbootstudy.responsibility;
+
+public class LeaveRequest {
+
+    /**
+     * 请假人姓名
+     */
+    private String name ;
+
+    /**
+     * 请假内容
+     */
+    private String content ;
+
+    /**
+     * 请假天数
+     */
+    private Integer dayNumber ;
+}
